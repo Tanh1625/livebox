e6360c27520e21ef6d72d6bc8712da3aeab3ea4c
Nguyễn Phương Thảo <thaonguyen1142004@gmail.com>
2026-04-22 14:51:41 +0700

    add folder

--- livebox-frontend/src/features/server/api/serverApi.ts
+++
@@ -1,9 +0,0 @@
-import axiosInstance from '../../../config/axios';
-import { mapToServerResponse } from '../types/server.types';
-import type { ServerCreateRequest, ServerResponse, ApiResponse } from '../types/server.types';
-
-export const createServer = async (payload: ServerCreateRequest): Promise<ServerResponse> => {
-  // KHÔNG xử lý auth ở đây
-  const response = await axiosInstance.post<ApiResponse<ServerResponse>>('/servers', payload);
-  return mapToServerResponse(response.data.data);
-};
